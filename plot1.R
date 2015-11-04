494c3ed88023dc66e5ca1a1247e4921f9bdf47b6
mtbbiker <eugenet@netstar.altech.co.za>
2015-11-04 15:16:14 +0200

    Add file

--- plot1.R
+++ plot1.R
@@ -1,1 +1,13 @@
 #Add code here
+library(data.table)
+datafile <- "household_power_consumption.txt"
+# Get the Training Data
+pol_data <- read.table(datafile,sep=";",header = TRUE)
+
+library(dplyr)
+#x <- filter(pol_data, Date >= "2007-02-01" , Date  <= "2007-02-02")
+#Convert to POSIX Date and format
+pol_data$Date <- as.Date(strptime(pol_data$Date,"%d/%m/%Y"),"%Y-%m-%d" )
+#pol_data$Date <- strptime(pol_data$Date,"%d/%m/%Y")
+
+smalldata <- subset(pol_data, Date >= "2007-02-01" & Date <= "2007-02-02")
