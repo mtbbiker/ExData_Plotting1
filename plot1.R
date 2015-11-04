a26b0e86ea5dbc621a417350702740d23e964a6e
mtbbiker <eugenet@netstar.altech.co.za>
2015-11-04 16:21:33 +0200

    Update file

--- plot1.R
+++ plot1.R
@@ -10,7 +10,12 @@ library(dplyr)
 pol_data$Date <- as.Date(strptime(pol_data$Date,"%d/%m/%Y"),"%Y-%m-%d" )
 #pol_data$Date <- strptime(pol_data$Date,"%d/%m/%Y")
 
+#Format text to be coerced to numerics later
+pol_data$Global_active_power <- as.numeric(gsub(",","",pol_data$Global_active_power))
+#pol_data$Global_active_power <- as.numeric(pol_data$Global_active_power)
+
 smalldata <- subset(pol_data, Date >= "2007-02-01" & Date <= "2007-02-02")
+smalldata$Global_active_power <- as.numeric(smalldata$Global_active_power)
 
 #hist(as.numeric(smalldata$Global_active_power), col = "orange",breaks = 100)
-hist(as.numeric(smalldata$Global_active_power), col = "orange",breaks = 100)
+hist(smalldata$Global_active_power, col = "orange",main = "Global Active Power",xlab = "Global Active Power(kilowatts)")
